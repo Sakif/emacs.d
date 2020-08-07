2e1527e06943b98ac394f170d55e45e7dfc4bc7a
Sakif Fahmid Zaman <smfzaman@gmail.com>
2020-08-07 10:48:16 -0300

    no cpp

--- init.el
+++ init.el
@@ -1,29 +1,21 @@
 
-;;; Code:
+;;;Code:
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; custom - set - variables was added by Custom. If you edit it by hand, you could mess it up, so be careful. Your init file should contain only one such instance. If there is more than one, they won't work right.
  '(cua-mode t nil (cua-base))
  '(inhibit-startup-screen t)
  '(make-backup-files nil)
- '(package-selected-packages
-   (quote
-    (flycheck-irony irony helm-gtags doom-themes doom-modeline)))
+ '(package-selected-packages (quote(iedit flycheck-irony irony helm-gtags doom-themes doom-modeline)))
  '(spaceline-helm-mode t)
  '(spaceline-show-default-input-method t))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ ;; custom - set - faces was added by Custom. If you edit it by hand, you could mess it up, so be careful. Your init file should contain only one such instance. If there is more than one, they won't work right.
+)
 
 (load "~/.emacs.d/general/base-setup.el")
 (load "~/.emacs.d/general/theme.el")
 (load "~/.emacs.d/general/mode-line.el")
-(load "~/.emacs.d/c_cpp/setup.el")
+;(load "~/.emacs.d/c_cpp/setup.el")
 
 ;;; init.el ends here
