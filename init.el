9932fd1c2a2824c97eeeac4726f560aacdf11944
Sakif Zaman <smfzaman@gmail.com>
2020-12-29 23:39:46 -0400

    more config

--- init.el
+++ init.el
@@ -1,12 +1,28 @@
-
+;;; package --- Summary
+;;; Commentary:
+;;; my Emacs config file
 ;;; Code:
+(setq user-full-name "Sakif Fahmid Zaman" ; who am I?
+      user-mail-address "smfzaman@gmail.com")
+
 (setq inhibit-startup-message t) ; no start up message
 (menu-bar-mode -1) ; no menubar
 (global-visual-line-mode) ; wraping
-(show-paren-mode) ; highlight paran
-(setq visible-bell t) ; no sound
-(column-number-mode)
+(column-number-mode) ; shoes the column number
+; (global-hl-line-mode +1) ; highlights line number
+; (line-number-mode +1) ; line numbers
+; (global-display-line-numbers-mode 1) ; more line numbers
+(size-indication-mode t) ; indicates the current tab size
+; to stop writing out the full yes and no
+(fset 'yes-or-no-p 'y-or-n-p)
 
+(global-auto-revert-mode t) ; automatically reloads buffer if changes made outside of emacs
+
+(setq-default tab-width 4 ; set tab size
+              indent-tabs-mode nil)
+
+;; clean up whitespace before save
+(add-hook 'before-save-hook 'whitespace-cleanup)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,7 +31,7 @@
  '(cua-mode t nil (cua-base))
  '(package-selected-packages
    (quote
-    (iedit org-mode helm-ispell magit helpful which-key rainbow-delimiters doom-themes doom-modeline flycheck helm company use-package))))
+    (ccls company-lsp helm-rg iedit helm-ispell magit helpful which-key rainbow-delimiters doom-themes doom-modeline flycheck helm company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,8 +41,7 @@
 
 ;; initialize package
 (require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 
@@ -37,26 +52,50 @@
   :custom
   (use-package-always-ensure t))
 
+(use-package smartparens
+  :config
+  (smartparens-global-mode 1)
+  (require 'smartparens-config)
+  (show-paren-mode t))
+
 ;; helm
-(use-package helm
+(use-package helm-projectile
   :init
   (global-unset-key (kbd "C-x c"))
+  ;; set prijectile home directory
+  (when (file-directory-p "~/code")
+    (setq projectile-project-search-path '("~/code")))
+  :custom
+  (helm-autoresize-max-height 0)
+  (helm-autoresize-min-height 20)
   :config
   (require 'helm-config)
+  (projectile-mode +1)
   (helm-mode 1)
+  (helm-projectile-on)
+  (helm-autoresize-mode 1)
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
   :bind
   (("M-x" . helm-M-x)
    ("C-x b" . helm-mini)
    ("M-y" . helm-show-kill-ring)
    ("C-x C-f" . helm-find-files)))
 
+;; for spelling
 (use-package helm-ispell)
 
+;; language server protocol
+(use-package lsp-ui)
+
 ;; company
-(use-package company
-  :diminish
+(use-package company-lsp
   :config
   (global-company-mode)
+  (push 'company-lsp company-backends)
   :custom
   (company-idle-delay 0)
   (company-minimum-prefix-length 1)
@@ -88,13 +127,26 @@
 
 ;; colourful delim
 (use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
 
+;; tells which function is binded to which keyboard shortcut
 (use-package which-key
   :init
   (which-key-mode)
   :custom
   (which-key-idle-delay 1))
 
+;; for finding all in buffer and replacing them
 (use-package iedit)
+
+;; cpp
+(use-package ccls
+  :config
+  (setq ccls-executable "ccls")
+  (setq lsp-prefer-flymake nil)
+  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+  :hook
+  ((c-mode c++-mode) . (lambda () (require 'ccls) (lsp))))
+
 ;;; init.el ends here
