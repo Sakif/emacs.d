c372434fa3dded6e43082ad5a39ede2d5a04c39a
Sakif Zaman <smfzaman@gmail.com>
2021-04-02 15:33:23 -0300

    bash

---
+++ bash.sh
@@ -0,0 +1,19 @@
+#/bin/env bash
+PS1="\W\$ "
+
+alias cls="clear"
+alias zstd="zstdmt -v --rm --ultra -22"
+alias ed="emacs -nw"
+alias xmlf="xmlformat --overwrite"
+alias xmlc="xmlformat --compress --overwrite"
+
+cb () {
+    tar cf "${PWD##*/}.cbt" * &&
+    mv -v "${PWD##*/}.cbt" ..;
+}
+
+tzst () {
+    tar cf "${PWD##*/}.tar" * &&
+    zstdmt -v --rm --ultra -22 "${PWD##*/}.tar" &&
+    mv -v "${PWD##*/}.tar.zst" ..;
+}
