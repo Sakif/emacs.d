06721672476997f5bafd396b53caec34e3bd811e
Sakif Zaman <smfzaman@gmail.com>
2021-04-03 00:35:54 -0300

    youtube and html

--- init.el
+++ init.el
@@ -28,8 +28,9 @@
   (use-package-always-ensure t))
 
 (use-package doom-themes
+  ;; theme
   :config
-  (load-theme 'doom-nord t))
+  (load-theme 'doom-vibrant t))
 
 (use-package doom-modeline
   ;; better mode line
@@ -116,10 +117,26 @@
   (c++-mode . eglot-ensure)
   (before-save . eglot-format-buffer)
   :config
-  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd-11"))
   :custom
   (eglot-ignored-server-capabilites (quote (:documentHighlightProvider))))
 
 (use-package python-mode
   :hook
   (python-mode . eglot-ensure))
+
+(use-package company-web)
+(use-package html5-schema)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(html5-schema company-web which-key use-package smartparens rainbow-delimiters python-mode iedit helm-projectile eglot doom-themes doom-modeline company auto-package-update)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- youtube.sh
+++ youtube.sh
@@ -1,3 +1,3 @@
 #/bin/env bash
-youtube-dl --output "%(title)s.%(ext)s" --format "bestvideo[height<1444]+bestaudio/best"
+youtube-dl --format "bestvideo[height<1444]+bestaudio/best" --output "%(title)s.%(ext)s" --geo-bypass
 # --playlist-start 1 --playlist-end 1 --output "%(playlist_index)s.%(ext)s"
